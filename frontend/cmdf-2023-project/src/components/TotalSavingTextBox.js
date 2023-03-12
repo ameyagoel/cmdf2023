0a5a85eadd834bc24cd606c95474becf6196f73d
zhanginc <zhanginc@students.cs.ubc.ca>
2023-03-11 22:45:38 -0800

    adds more frontend components

---
+++ frontend/cmdf-2023-project/src/components/TotalSavingTextBox.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const OuterContainer = styled.div`
+    text-align: center;
+    align-items: center;
+    dispaly: flex;
+    justify-content: center;
+    border-radius: 10px;
+    width: 30em;
+    height: 4em;
+    background-color: #e0adc6;
+    position: relative;
+    padding-top: 1em; 
+`
+const Heading = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  line-height: 1.1;
+  font-weight: 600;
+  color: #CC719E;
+  `
+
+
+export const TotalSavingTextBox = ({total}) => {
+    return(
+        <OuterContainer>
+        <Heading>Total Saved: {total} </Heading>        
+        </OuterContainer>
+
+    );
+
+
+}
